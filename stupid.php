de973f07c34a216f4328807cadf68d2355800f73
vilotacks <73704894+vilotacks@users.noreply.github.com>
2020-10-31 23:14:08 +0000

    Create stupid.php

---
+++ stupid.php
@@ -0,0 +1,10 @@
+<html>
+<head><title>um test</title></head>
+<body>
+
+<form action="process.php" method="post">
+Enter your name: <input name="asdf" type="text">
+<input type="submit">
+</form>
+</body>
+</html>
